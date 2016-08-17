c7de23b9e0e2129614d7189c05a7d5ee73b439d6
myc1995 <875940302@qq.com>
2016-08-17 17:51:59 +0800

    myc1995 - add doctor for reducers

--- public/src/reducers/doctor.js
+++ public/src/reducers/doctor.js
@@ -1,3 +1,9 @@
-/**
- * Created by myc on 8/17/16.
- */
+const doctor = (state = [], action)=> {
+  switch (action.type) {
+    case 'DOCTOR_LOADED':
+      return action.data;
+  }
+  return state;
+};
+
+export default doctor;
