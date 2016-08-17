df45d1c49dd748fdc4dd28888d181ad2e3ad0421
myc1995 <875940302@qq.com>
2016-08-17 18:19:20 +0800

    myc1995 - change import index.js to doctor.js

--- public/src/user_board.js
+++ public/src/user_board.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Doctor from './container/Doctor';
-import doctor from './reducers';
+import doctor from './reducers/doctor';
 
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
