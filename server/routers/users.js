98ace91f7571550dcbe43702c13764f181c91e9c
hang515161153 <515161153@qq.com>
2016-08-17 23:03:36 +0800

    hanzi - sever_user

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,3 +1,32 @@
-/**
- * Created by weihang on 16-8-17.
- */
+import express from 'express';
+import User from '../models/User';
+
+let router = express.Router();
+
+router.get('/', (req, res)=> {
+  User.find((err, data)=> {
+    res.send(data);
+  });
+});
+
+router.get('/:username&:password', (req, res)=> {
+  User.findOne({
+    username:req.params.username,
+    password:req.params.password
+  }, (err, data)=> {
+    if(data) {
+      res.send({
+        result:true,
+        user_id:data._id
+      });
+    } else {
+      res.send({
+        result:false,
+        user_id:null
+      });
+    }
+
+  });
+});
+
+module.exports = router;
