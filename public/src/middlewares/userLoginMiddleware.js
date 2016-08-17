4033ea0824e7026ea96f8bb3e2f13928261423e5
hang515161153 <515161153@qq.com>
2016-08-17 23:00:56 +0800

    hanzi - userLoginMiddleware

--- public/src/middlewares/userLoginMiddleware.js
+++ public/src/middlewares/userLoginMiddleware.js
@@ -1,4 +1,16 @@
 import request from 'superagent';
-const userReqestMiddleware = store=>next=>action=>{
-
+const userLoginMiddleware = store=>next=>action=>{
+  switch (action.type){
+    case 'INIT':
+      request.get('/api/doctors')
+          .end((err, res)=> {
+            next({
+              type: 'SUCCESS',
+              data: res.body
+            });
+          });
+      break;
+  }
+  next(action);
 };
+export default userLoginMiddleware;
