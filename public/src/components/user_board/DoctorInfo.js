876898936489f160e71a40bff35e622cafde8394
myc1995 <875940302@qq.com>
2016-08-17 17:46:03 +0800

    myc1995 - add DoctorInfo component

--- public/src/components/user_board/DoctorInfo.js
+++ public/src/components/user_board/DoctorInfo.js
@@ -1,3 +1,25 @@
-/**
- * Created by myc on 8/17/16.
- */
+import React, {Component} from "react";
+
+class DoctorInfo extends Component {
+  render() {
+    console.log(this.props);
+    let {doctor_name, image, email, type, hospital, certificaties}=this.props.doctor;
+
+    return (
+        <div classID="doctor_info">
+          <ul>
+            <label>医生信息</label>
+            <li>
+              <img className="doctor_img" src={image} alt="医生图片"/>
+            </li>
+            <li>姓名:{doctor_name}</li>
+            <li>e-mail:{email}</li>
+            <li>科室:{type}</li>
+            <li>所在医院:{hospital}</li>
+            <li>级别:{certificaties}</li>
+          </ul>
+        </div>)
+  }
+}
+
+export default DoctorInfo;
