02af4eb164e979040731681b49b3e0a91f8ccfe2
myc1995 <875940302@qq.com>
2016-08-17 18:22:48 +0800

    myc1995 - fix find a doctor_spec

--- server/routers/doctors.js
+++ server/routers/doctors.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import Doctor from '../models/Doctor';
+import express from "express";
+import Doctor from "../models/Doctor";
 
 let router = express.Router();
 
@@ -13,7 +13,12 @@ router.get('/:id', (req, res)=> {
   Doctor.findOne({
     _id: req.params.id
   }, (err, data)=> {
-    res.send(data);
+    if (data) {
+      res.send(data);
+
+    } else {
+      res.sendStatus(404);
+    }
   });
 });
 
