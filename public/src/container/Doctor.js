17cd80ccc3461f7b93499e0ac1926b27be228c1d
myc1995 <875940302@qq.com>
2016-08-17 17:49:35 +0800

    myc1995 - add Doctor for container

--- public/src/container/Doctor.js
+++ public/src/container/Doctor.js
@@ -1,3 +1,11 @@
 /**
  * Created by myc on 8/17/16.
  */
+import { connect } from 'react-redux';
+import Doctor from '../components/user_board/DoctorInfo';
+
+const mapStateToProps = (state)=> (state);
+
+const DoctorInfo = connect(mapStateToProps)(Doctor);
+
+export default DoctorInfo;
