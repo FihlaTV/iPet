9956d92e4414ce6f7747c0f911336ce3f8e34fc6
hang515161153 <515161153@qq.com>
2016-08-17 14:07:37 +0800

    hanzi - modify Head Component

--- public/src/components/Head.js
+++ public/src/components/Head.js
@@ -6,15 +6,18 @@ import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 class Head extends Component {
   render() {
     return (
-        <div className="row head">
-          <div className="col-xs-4 col-md-1">
-            <div className="thumbnail">
-              <img src="images/logo.jpg" alt="iPet logo"></img>
-            </div>
-
-          </div>
-          <div>person return exit</div>
+      <div className="head">
+        <div className="headLeft">
+          <img className="logo" src="images/login_images/ipetlogo.png" alt="iPet logo" />
+          <span className="logoName">宠物之家</span>
         </div>
+        <div className="headRight">
+          <span className="userImage">
+            <img src="images/dog.jpg" alt="用户头像"/>
+          </span>
+          <span className="logIn">注销</span>
+        </div>
+      </div>
     );
   }
 }
