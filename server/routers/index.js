9a695432597c6a2f3e69b9d788efafea026f0887
myc1995 <875940302@qq.com>
2016-08-18 17:40:09 +0800

    huxiongchao - change router index.js for use user

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,5 +1,6 @@
 const regRouter = (app)=> {
   app.use('/api/doctors', require('./doctors'));
   app.use('/api/login', require('./users'));
+  app.use('/api/users',require('./users'));
 };
 export default regRouter;
