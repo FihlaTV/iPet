112fe7d02f11343269dfdec43dcada0fc4e081f5
myc1995 <875940302@qq.com>
2016-08-18 17:52:18 +0800

    huxiongchao - change index.js on reducers

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,10 +1,8 @@
 import {combineReducers} from 'redux';
 import doctors from './doctors';
-// import doctor from './doctor';
 
 const resultList = combineReducers({
   doctors
-  // doctor
 });
 
 export default resultList;
