f1da83b97bca2e01685db692f10f9b0adee123c4
karida-bean <1816063167@qq.com>
2016-08-18 18:28:44 +0800

    zhyingjia - add user message reduce

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import doctors from './doctors';
 // import doctor from './doctor';
+import messages from './messages';
 
 const resultList = combineReducers({
-  doctors
+  doctors,
+  messages
   // doctor
 });
 
