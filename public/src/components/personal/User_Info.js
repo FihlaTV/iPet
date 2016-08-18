de052e02ec2e0bc45d3dad108c97cba963267496
myc1995 <875940302@qq.com>
2016-08-18 17:42:42 +0800

    huxiongchao - add User_info for Component

--- public/src/components/personal/User_Info.js
+++ public/src/components/personal/User_Info.js
@@ -1,3 +1,24 @@
 /**
  * Created by myc on 8/18/16.
  */
+import React, {Component} from "react";
+
+class User_Info extends Component {
+  render() {
+    console.log(this.props);
+    let {username, email, description}=this.props;
+
+    return (
+        <div classID="userInfo">
+          <p>个人信息</p>
+          <ul>
+            <li><img className="user_img" src="images/1.jpg" alt="img not found"/></li>
+            <li>用户名:{username}</li>
+            <li>e-mail:{email}</li>
+            <li>个人描述:{description}</li>
+          </ul>
+        </div>)
+  }
+}
+
+export default User_Info;
