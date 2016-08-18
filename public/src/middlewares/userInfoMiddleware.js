004532433fc794eb74fbd7fd4101f3673dbb7e56
myc1995 <875940302@qq.com>
2016-08-18 17:47:15 +0800

    huxiongchao - add userInfoMiddleware to middleware for action

--- public/src/middlewares/userInfoMiddleware.js
+++ public/src/middlewares/userInfoMiddleware.js
@@ -1,3 +1,21 @@
 /**
- * Created by myc on 8/18/16.
+ * Created by myc on 8/17/16.
  */
+import request from "superagent";
+
+const userInfoMiddleware = store =>next =>action=> {
+  switch (action.type) {
+    case 'INIT':
+      request.get('/api/users/47b2c7b9d8e1ecbf54300010')
+          .end((err, res)=> {
+            next({
+              type: 'USER_LOADED',
+              data: res.body
+            });
+          });
+      break;
+  }
+  next(action);
+};
+
+export default userInfoMiddleware;
