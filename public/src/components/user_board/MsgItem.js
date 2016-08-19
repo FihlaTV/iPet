9d35bc3c3f68435b7586f9270197f89c4e1bb73a
Yingjia Zhang <zhyingjia@163.com>
2016-08-18 22:35:42 -0400

    zhyingjia - modify MsgItem

--- public/src/components/user_board/MsgItem.js
+++ public/src/components/user_board/MsgItem.js
@@ -3,13 +3,18 @@
  */
 import React, {Component} from 'react';//eslint-disable-line no-unused-vars
 class MsgItem extends Component {
-  render() {
-    let {isQuestion, msg} = this.props;
-    return (
-        <div>
-          <pre >{isQuestion ? '问' : '答'}:{msg}</pre>
-        </div>
-    );
-  }
+    render() {
+        let {isQuestion, msg} = this.props;
+
+        if (isQuestion === undefined) {
+            return (<span></span>);
+        }
+
+        return (
+            <div>
+                <pre >{isQuestion ? '问' : '答'}:{msg}</pre>
+            </div>
+        );
+    }
 }
 export default MsgItem;
