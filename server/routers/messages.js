5ca9d44de36336cbd56f0c6afb6cd755486f6401
Yingjia Zhang <zhyingjia@163.com>
2016-08-21 13:12:14 -0400

    zhyingjia - delete get /find and get / & add get /load

--- server/routers/messages.js
+++ server/routers/messages.js
@@ -5,33 +5,30 @@
 import express from 'express';// eslint-disable-line no-unused-console
 import Message from '../models/Message';
 
-
 let router = express.Router();
-
-router.get('/', (req, res)=> {
-  Message.find((err, data)=> {
-    res.send(data);
-  });
-});
 
 router.put('/', (req, res)=> {
   Message.findById(req.query.id, (err, message)=> {
     message.talk.push(req.body);
-    Message.update({_id: message._id}, message, (err, data)=> {  // eslint-disable-line no-unused-vars
-      res.send({
-        error: err,
-        data: message.talk
-      });
+    Message.update({_id: message._id}, message, ()=> {
+      res.send(message);
     });
   });
 });
 
-router.get('/find', (req, res)=> {
-  Message.findById(req.query.id, (err, message)=> {
-    res.send({
-      error: err,
-      data: message
-    });
+router.get('/load', (req, res)=> {
+  Message.findOne({
+    userId: req.query.userId,
+    doctorId: req.query.doctorId
+  }, (err, message)=> {
+    message ?
+        res.send(message) :
+        new Message({
+          userId: req.query.userId,
+          doctorId: req.query.doctorId
+        }).save((err, msg)=> {
+          res.send(msg);
+        });
   });
 });
 
