10d69530c1568bf9715ec16ec72c3d8a1eea5b72
Yingjia Zhang <zhyingjia@163.com>
2016-08-21 19:52:28 -0400

    zhyingjia - format eslint

--- public/src/middlewares/userRequestMiddleware.js
+++ public/src/middlewares/userRequestMiddleware.js
@@ -7,31 +7,31 @@ import request from 'superagent';
 const userRequestMiddleware = store => next => action=> { // eslint-disable-line no-unused-vars
 
   switch (action.type) {
-    case 'ADD_USER_MSG':
-      request.put('/api/messages')
-          .type('form')
-          .query({id: action.msg_id})
-          .send(action.data)
-          .end((err, res)=> {
-            next({
-              type: 'LOAD_MSG',
-              data: res.body
-            });
-          });
-      break;
-
-    case 'MSG_INIT':
-      request.get('/api/messages/load')
-          .query({
-            userId: action.userId,
-            doctorId: action.doctorId
-          }).end((err, res)=> {
+  case 'ADD_USER_MSG':
+    request.put('/api/messages')
+        .type('form')
+        .query({id: action.msg_id})
+        .send(action.data)
+        .end((err, res)=> {
           next({
             type: 'LOAD_MSG',
             data: res.body
           });
         });
-      break;
+    break;
+
+  case 'MSG_INIT':
+    request.get('/api/messages/load')
+        .query({
+          userId: action.userId,
+          doctorId: action.doctorId
+        }).end((err, res)=> {
+          next({
+            type: 'LOAD_MSG',
+            data: res.body
+          });
+        });
+    break;
   }
   next(action);
 };
