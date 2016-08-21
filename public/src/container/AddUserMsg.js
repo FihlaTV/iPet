288cc2d60996a2ac24612ab345313e9c8096b7e9
Yingjia Zhang <zhyingjia@163.com>
2016-08-21 13:05:16 -0400

    zhyingjia - add messageInit to mapDispatchToProps

--- public/src/container/AddUserMsg.js
+++ public/src/container/AddUserMsg.js
@@ -1,11 +1,15 @@
 import {connect} from 'react-redux';
 import UserMessageBoard from '../components/user_board/UserMessageBoard';
 import {addUserMsg} from '../actions/addUserMsgAction';
+import {messageInit} from '../actions/messageInit'
 
 const mapStateToProps = (state)=>(state);
 const mapDispatchToProps = (dispatch)=>({
-  addUserMsg: (text)=> {
-    dispatch(addUserMsg(text));
+  addUserMsg: (text,msg_id)=> {
+    dispatch(addUserMsg(text,msg_id));
+  },
+  messageInit:(userId,doctorId)=>{
+    dispatch(messageInit(userId,doctorId));
   }
 });
 
