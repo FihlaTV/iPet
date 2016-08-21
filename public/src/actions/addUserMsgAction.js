e67a1d16a50e9094c63d40884c2271dd8007c14b
Yingjia Zhang <zhyingjia@163.com>
2016-08-21 12:59:51 -0400

    zhyingjia - add a property msg_id

--- public/src/actions/addUserMsgAction.js
+++ public/src/actions/addUserMsgAction.js
@@ -1,8 +1,9 @@
 /**
  * Created by zhangyiru on 16-8-16.
  */
-export const addUserMsg = (text)=>({
+export const addUserMsg = (text, msg_id)=>({
   type: 'ADD_USER_MSG',
+  msg_id: msg_id,
   data: {
     isQuestion: true,
     msg: text
