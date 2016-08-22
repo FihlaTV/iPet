d320e042465d0c221251ceff43c850cc3a83e9d3
Yingjia Zhang <zhyingjia@163.com>
2016-08-21 23:16:30 -0400

    qiyanzi - merge DoctorInfo and UserMsgBoardApp

--- public/src/components/user_board/UserBoard.js
+++ public/src/components/user_board/UserBoard.js
@@ -1,3 +1,26 @@
 /**
  * Created by sun on 8/21/16.
  */
+import React, {Component} from 'react';// eslint-disable-line no-unused-vars
+import DoctorInfo from '../../container/Doctor';
+import UserMsgBoard from '../../container/AddUserMsg';
+import Head from '../Head';
+import Foot from '../Foot';
+
+class UserBoard extends Component {
+
+  render() {
+    return (
+        <div>
+          <Head/>
+          <div>
+            <DoctorInfo doctor_id={this.props.params.doctor_id}/>
+            <UserMsgBoard/>
+          </div>
+          <Foot/>
+        </div>
+    );
+  }
+}
+
+export default UserBoard;
