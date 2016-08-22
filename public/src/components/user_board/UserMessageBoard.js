7736ca961a161114d216ae880be8f1bf21fac150
Yingjia Zhang <zhyingjia@163.com>
2016-08-21 23:19:56 -0400

    qiyanzi - format

--- public/src/components/user_board/UserMessageBoard.js
+++ public/src/components/user_board/UserMessageBoard.js
@@ -31,18 +31,20 @@ class UserMessageBoard extends Component {
   render() {
     let talk = this.props.messages.talk ? this.props.messages.talk : [];
     return (
-          <div id="right" className="col-sm-8">
-            <div className="all_message" ref="history">
-              {
-                talk.map((v, k)=>(
-                    <MsgItem key={k + 1} isQuestion={v.isQuestion} msg={v.msg}></MsgItem>
-                ))
-              }
-            </div>
-            <textarea id="sendMessage" ref="message"></textarea><br/>
-            <input type="button" onClick={this.handleClick.bind(this)} value="提交"/>
-            <input type="button" value="结束对话"/>
+
+        <div id="right" className="col-sm-8">
+          <div className="all_message" ref="history">
+            {
+              talk.map((v, k)=>(
+                  <MsgItem key={k + 1} isQuestion={v.isQuestion} msg={v.msg}></MsgItem>
+              ))
+            }
           </div>
+          <textarea id="sendMessage" ref="message"></textarea><br/>
+          <input type="button" onClick={this.handleClick.bind(this)} value="提交"/>
+          <input type="button" value="结束对话"/>
+        </div>
+
     );
   }
 }
