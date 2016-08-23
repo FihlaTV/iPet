092374fa4ac85f60b11e06664844b0c0b8686a40
Miss-qi <1370322806@qq.com>
2016-08-23 22:27:16 +0800

    qiyanzi - delete cat.png

--- public/src/components/Head.js
+++ public/src/components/Head.js
@@ -6,21 +6,17 @@ import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 class Head extends Component {
   render() {
     return (
-      <div className="head">
+        <div className="head">
           <img className='login-logo' src='images/login_images/ipetlogo.png'/>
           <span className='wareName'> </span>
-        <div className="headLeft">
-          <img className="logo" src="/images/login_images/ipetlogo.png" alt="iPet logo" />
-          <span className="logoName">宠物之家</span>
-        </div>
-        <div className="headRight">
-          {/*<img src="images/cat.png" className="head_pet" alt=""/>*/}
-          <span className="userImage">
+          <div className="headRight">
+            {/*<img src="images/cat.png" className="head_pet" alt=""/>*/}
+            <span className="userImage">
             <img src="/images/dog.jpg" alt="用户头像"/>
           </span>
-          <span className="logIn">注销</span>
+            <span className="logIn">注销</span>
+          </div>
         </div>
-      </div>
     );
   }
 }
