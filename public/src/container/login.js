821677fbd17da9ba4c04a7d0ddb98a7521e26fbb
xiongchaohu <1732139715@qq.com>
2016-08-24 23:59:55 +0800

    huxiongchao - change login.js to add signIn action and clearStatus

--- public/src/container/login.js
+++ public/src/container/login.js
@@ -5,6 +5,8 @@ import {connect} from 'react-redux';
 import Content from '../components/index_components/index_content';
 import  {loginTodo}  from '../actions';
 import {showLoginBoard} from '../actions/showLoginBoard';
+import {signIn} from '../actions/signInAction';
+import {clearStatus} from '../actions/clearStatusAction';
 
 const mapStateToProps = (state)=> (state);
 const mapDispatchToProps = (dispatch)=>({
@@ -14,6 +16,14 @@ const mapDispatchToProps = (dispatch)=>({
 
   showLoginBoard: (text)=> {
     dispatch(showLoginBoard(text));
+  },
+
+  signIn: (data)=> {
+    dispatch(signIn(data));
+  },
+
+  clearStatus:()=>{
+    dispatch(clearStatus());
   }
 });
 const LoginView = connect(mapStateToProps, mapDispatchToProps)(Content);
