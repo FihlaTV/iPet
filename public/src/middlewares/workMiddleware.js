14363488c10c9ab5f3ad82f118086bf218bcde02
Miss-qi <1370322806@qq.com>
2016-08-24 22:24:17 +0800

    qiyanzi - add store.dispatch

--- public/src/middlewares/workMiddleware.js
+++ public/src/middlewares/workMiddleware.js
@@ -23,6 +23,7 @@ const workMiddleware = store=>next=>action=> {   // eslint-disable-line no-unuse
           .query({id: action.msg_id})
           .send(action.data)
           .end((err, res)=> {
+            store.dispatch({type:'WORK_INIT'})
             next({
               type: 'LOAD_DOCTOR_MSG',
               data: res.body
