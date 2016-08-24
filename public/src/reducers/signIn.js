ef1bafc2b740153f3b24b32c1faff029b42ea6a8
xiongchaohu <1732139715@qq.com>
2016-08-24 23:57:48 +0800

    add reducer signIn

--- public/src/reducers/signIn.js
+++ public/src/reducers/signIn.js
@@ -4,8 +4,6 @@
 const user_sign_in = (state = '', action)=> {
   switch (action.type) {
     case 'SIGN_IN_RESULT':
-      /*console.log('+++++++++++++++++++++');
-      console.log(action.isSuccess);*/
       return action.isSuccess;
     case 'CLEAR_STATUS':
       console.log(action.type);
